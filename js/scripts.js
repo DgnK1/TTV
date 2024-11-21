55ad428e61daac257507ee9b23979eef1c3ba39e
DgnK1 <133643470+DgnK1@users.noreply.github.com>
2024-11-21 11:18:57 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+modal = document.getElementById('id01'),
+overlay = document.getElementById('overlay'); 
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  document.getElementById('id01').style.display = 'block';
+  
+  const logoutBtn = document.getElementById('logoutBtn');
+  if (logoutBtn) {
+      logoutBtn.addEventListener('click', function() {
+          alert('You have been logged out.');
+          location.reload();
+      });
+  }
+});
+
+function closeModal() {
+  modal.style.backgroundColor = 'rgba(0,0,0,0)'; 
+  document.querySelector('.modal-content').style.opacity = '0'; 
+  
+  modal.style.display = 'none'; 
+  overlay.style.display = 'none'; ;
+}
+
+document.getElementById('login-form').addEventListener('submit', function(e) {
+  e.preventDefault(); 
+  handleLoginSuccess(); 
+});
+
+function handleLoginSuccess() {
+  document.querySelector('.content').classList.remove('hidden');
+  closeModal(); 
+}
+
+function showSignUp() {
+  document.getElementById('login-form').style.display = 'none';
+  document.getElementById('sign-up-form').style.display = 'block';
+}
+
+function showLogin() {
+  document.getElementById('sign-up-form').style.display = 'none';
+  document.getElementById('login-form').style.display = 'block';
+}
+
+function signUp() {
+  const username = document.getElementById('new-username').value;
+  const email = document.getElementById('new-email').value;
+  const password = document.getElementById('new-password').value;
+  
+  if (username && password) {
+      localStorage.setItem('username', username);
+      localStorage.setItem('email', email);
+      localStorage.setItem('password', password);
+      alert('Sign Up successful! You can now log in.');
+      showLogin();
+  } else {
+      alert('Please fill in all the fields.');
+  }
+}
+
+function login() {
+  const userInput = document.getElementById('login-username-email').value;
+  const password = document.getElementById('login-password').value;
+  
+  const storedUsername = localStorage.getItem('username');
+  const storedEmail = localStorage.getItem('email');
+  const storedPassword = localStorage.getItem('password');
+  
+  if ((userInput === storedUsername || userInput === storedEmail) && password === storedPassword) {
+      alert('Login successful!');
+      closeModal();
+  } else {
+      alert('Invalid credentials.');
+  }
+}
